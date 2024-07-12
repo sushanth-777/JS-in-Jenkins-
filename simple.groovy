9cd10355058b7ba3fe1cdc41292d53df6b1efc32
Sushanth Kotha <sushanth@Sushanths-MacBook-Air.local>
2024-07-12 17:15:20 +0530

    corrected some syntax errors

--- simple.groovy
+++ simple.groovy
@@ -1,29 +1,28 @@
 pipeline {
     agent any
-
+    
     environment {
-        GIT_CREDENTIALS = credentials('your-credentials-id')
+        GIT_CREDENTIALS = credentials('15eaaf77-b157-4c3d-9574-a88faeea5805')
     }
-
     stages {
         stage('Build') {
             steps {
-                git branch: 'main', credentials: GIT_CREDENTIALS, url: 'https://github.com/sushanth-777/JS-in-Jenkins-'
+                git branch: 'main', credentialsId: GIT_CREDENTIALS, url: 'https://github.com/sushanth-777/JS-in-Jenkins-'
+                git 'https://github.com/sushanth-777/JS-in-Jenkins-'
                 sh 'npm install'
                 sh 'npm run build'
             }
         }
-
+        
         stage('Test') {
             steps {
                 sh 'npm run test'
             }
         }
     }
-
     post {
         always {
-            archiveArtifacts 'path/to/artifacts/**'
+            archiveArtifacts 'path/to/artifacts'
         }
     }
 }
