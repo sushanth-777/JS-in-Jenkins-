93362ba6385685ea695fefa3128063d737d40e1a
Sushanth Kotha <sushanth@Sushanths-MacBook-Air.local>
2024-07-12 17:09:58 +0530

    added github cresentials

--- simple.groovy
+++ simple.groovy
@@ -1,24 +1,29 @@
 pipeline {
     agent any
 
+    environment {
+        GIT_CREDENTIALS = credentials('your-credentials-id')
+    }
+
     stages {
         stage('Build') {
             steps {
-                git 'https://github.com/sushanth-777/JS-in-Jenkins-'
+                git branch: 'main', credentials: GIT_CREDENTIALS, url: 'https://github.com/sushanth-777/JS-in-Jenkins-'
                 sh 'npm install'
                 sh 'npm run build'
             }
         }
-        
+
         stage('Test') {
             steps {
                 sh 'npm run test'
             }
         }
     }
+
     post {
         always {
-            archiveArtifacts 'path/to/artifacts'
+            archiveArtifacts 'path/to/artifacts/**'
         }
     }
 }
