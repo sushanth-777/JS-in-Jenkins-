b3b381cd40903e8b156286ce05c71a1a2ca634f2
Sushanth Kotha <sushanth@Sushanths-MacBook-Air.local>
2024-07-13 22:45:46 +0530

    some changes in the groovy script

--- simple.groovy
+++ simple.groovy
@@ -1,28 +1,45 @@
 pipeline {
     agent any
-    
-    environment {
-        GIT_CREDENTIALS = credentials('15eaaf77-b157-4c3d-9574-a88faeea5805')
-    }
+
     stages {
+        stage('Checkout') {
+            steps {
+                // Pull code from GitHub
+                git branch: 'main', url: 'https://github.com/sushanth-777/JS-in-Jenkins-.git'
+                // List files to verify the checkout
+                sh 'ls -al'
+            }
+        }
+
         stage('Build') {
             steps {
-                git branch: 'main', credentialsId: GIT_CREDENTIALS, url: 'https://github.com/sushanth-777/JS-in-Jenkins-'
-                git 'https://github.com/sushanth-777/JS-in-Jenkins-'
-                sh 'npm install'
-                sh 'npm run build'
+                echo 'Building the project...'
+                // If there's a build step, include it here
+                // For static files, this might be empty
             }
         }
-        
+
         stage('Test') {
             steps {
-                sh 'npm run test'
+                echo 'Running tests...'
+                // If you have tests, include them here
+                // For static files, this might be empty
             }
         }
-    }
-    post {
-        always {
-            archiveArtifacts 'path/to/artifacts'
+
+        stage('Deploy') {
+            steps {
+                echo 'Deploying the project...'
+                // Serve the static files using a simple HTTP server
+                sh '''
+                    python3 -m http.server 1234 &
+                    while ! curl -s http://localhost:1234; do
+                        echo "Waiting for server to start..."
+                        sleep 1
+                    done
+                    echo "Deployment successful, application is running"
+                '''
+            }
         }
     }
 }
